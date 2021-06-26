a5029807eff59c9982f255ed592352511544064e
Seba Gomes <sebastian.gomes@gmail.com>
2021-06-26 13:03:59 -0300

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,6 @@ const Home = () => (
       <h3><a href="https://lacritica.com.ar/2020/06/25/ana-pedraza-tucumana-belleza/">Ana Pedraza</a></h3>
   
 
-      <h3><a href="https://argencon.org">argencon</a></h3>
-
-      <h3><a href="https://lacritica.com.ar/2021/02/02/martin-cirio-reaparecio-me-descarto-como-si-no-fuera-nada/">Martin Cirio</a></h3>
-
-      <h3>Mirá<a href="https://mercadoads.com">como vender mas en mercado libre</a></h3>
 
       <h3>Hooray 🎉 - Veamos sin funciona la <a href="https://lacritica.com.ar/quiniela-de-tucuman">Quiniela de Tucuman</a></h3>
 
@@ -30,13 +25,13 @@ const Home = () => (
 
 <h3><a href="https://lacritica.com.ar/2020/02/03/ana-pedraza-la-periodista-tucumana-que-cautiva-con-su-carisma-y-aman-en-buenos-aires/"> ana pedraza</a></h3>
 <h3><a href="https://lacritica.com.ar/clima-en-tucuman/">clima en Tucuman</a></h3>
-<h3><a href="https://lacritica.com.ar/quiniela-de-tucuman"><strong>quiniela de tucuman</strong></a></h3>
+<h3><a href="https://lacritica.com.ar/quiniela-de-tucuman">quiniela de tucuman</a></h3>
 <h3><a href="https://lacritica.com.ar/2020/04/27/es-la-senora-poron-el-tucu-lopez-mostro-de-mas-y-ardio-twitter/">tucu lopez</a></h3>
-<h3><a class="" href="https://lacritica.com.ar/2020/01/27/silvina-saleme-posleman-le-prendio-fuego-a-los-cerros-tucumanos-con-fotos-super-hot/">silvina saleme posleman</a></h3>
+<h3><a href="https://lacritica.com.ar/2020/01/27/silvina-saleme-posleman-le-prendio-fuego-a-los-cerros-tucumanos-con-fotos-super-hot/">silvina saleme posleman</a></h3>
 <h3><a href="https://lacritica.com.ar/2020/02/19/jorgelina-zalazar-vs-gladys-la-bomba-tucumana-que-susto-que-se-llevo-la-video/?swcfpc=1">jorgelina zalazar</a></h3>
 
 <h3><a href="http://telefonosatencioncliente.xyz">Telefonos de atencion al cliente</a></h3>
-<h3><a href="https://lacritica.netlify.app">netlify</a></h3>
+<h3><a href="https://tucumaneando.netlify.app">netlify</a></h3>
 <h3><a href="https://sites.google.com/view/tuquman/">tucuman</a></h3>
 <h3><a href="https://sebastiangomes5.wixsite.com/tuquman">tuc</a>/h3>
 <h3><a href="https://proyectoasistir.com.ar">piscologo online</a></h3>
@@ -63,7 +58,6 @@ const Home = () => (
 <h3><a href="https://people.mozilla.org/p/sebasg">Mozilla</a></h3>
 <h3><a href="https://www.google.es/url?q=https://lacritica.com.ar/"> ricardo bussi </a></h3>
 <h3><a href="https://spark.adobe.com/page/QGyPFWsmiJCTK/">adobe </a></h3>
-<h3><a href="marketingdigitalexperience.com/curso-de-marketing-digital/">curso de marketing digital </a></h3>
 <h3><a href="https://huffduffer.com/sebasg/610279">huffduffer</a></h3>
 <h3><a href="https://spacebabe.ams3.digitaloceanspaces.com/index.html">Spaces babe</a></h3>
 <h3><a href="https://epaepa.oss-ap-northeast-1.aliyuncs.com/index.html">Epaepadiscoestepa</a></h3>
