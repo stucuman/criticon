02eb84eec73104f3b2a7c825b65ef10bff486679
Seba Gomes <sebastian.gomes@gmail.com>
2021-06-26 12:57:19 -0300

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,87 @@ const Home = () => (
     <Head>
       <title>Next.js on the Jamstack with Netlify!</title>
     </Head>
-    <h1>
-      Next.js on the <a href="https://jamstack.org">Jamstack</a>
-    </h1>
+    
+  <div>
+    <h1>funciona</h1>
 
-    <h3>
-      Hooray 🎉 - you've built this with{' '}
-      <a href="https://nextjs.org">Next.js</a>!
-    </h3>
+    <h3>Hooray 🎉 - Veamos sin funciona la <a href="https://lacritica.com.ar/quiniela-de-tucuman">Quiniela de Tucumán</a></h3>
+      <h3><a href="https://lacritica.com.ar/2020/06/25/ana-pedraza-tucumana-belleza/">Ana Pedraza</a></h3>
+  
+
+      <h3><a href="https://argencon.org">argencon</a></h3>
+
+      <h3><a href="https://lacritica.com.ar/2021/02/02/martin-cirio-reaparecio-me-descarto-como-si-no-fuera-nada/">Martin Cirio</a></h3>
+
+      <h3>Mirá<a href="https://mercadoads.com">como vender mas en mercado libre</a></h3>
+
+      <h3>Hooray 🎉 - Veamos sin funciona la <a href="https://lacritica.com.ar/quiniela-de-tucuman">Quiniela de Tucuman</a></h3>
+
+      <h3>Hooray 🎉 - Veamos sin funciona la <a href="https://lacritica.com.ar/quiniela-de-tucuman">resultados Quiniela de Tucuman</a></h3>
+ 
+
+
+<h3><a href="http://lacritica.com.ar">noticias de tucuman</a></h3>
+<h3><a href="https://lacritica.com.ar/clima-en-tucuman/">Clima en Tucuman</a></h3>
+
+<h3><a href="https://lacritica.com.ar/2020/02/03/ana-pedraza-la-periodista-tucumana-que-cautiva-con-su-carisma-y-aman-en-buenos-aires/"> ana pedraza</a></h3>
+<h3><a href="https://lacritica.com.ar/clima-en-tucuman/">clima en Tucuman</a></h3>
+<h3><a href="https://lacritica.com.ar/quiniela-de-tucuman"><strong>quiniela de tucuman</strong></a></h3>
+<h3><a href="https://lacritica.com.ar/2020/04/27/es-la-senora-poron-el-tucu-lopez-mostro-de-mas-y-ardio-twitter/">tucu lopez</a></h3>
+<h3><a class="" href="https://lacritica.com.ar/2020/01/27/silvina-saleme-posleman-le-prendio-fuego-a-los-cerros-tucumanos-con-fotos-super-hot/">silvina saleme posleman</a></h3>
+<h3><a href="https://lacritica.com.ar/2020/02/19/jorgelina-zalazar-vs-gladys-la-bomba-tucumana-que-susto-que-se-llevo-la-video/?swcfpc=1">jorgelina zalazar</a></h3>
+
+<h3><a href="http://telefonosatencioncliente.xyz">Telefonos de atencion al cliente</a></h3>
+<h3><a href="https://lacritica.netlify.app">netlify</a></h3>
+<h3><a href="https://sites.google.com/view/tuquman/">tucuman</a></h3>
+<h3><a href="https://sebastiangomes5.wixsite.com/tuquman">tuc</a>/h3>
+<h3><a href="https://proyectoasistir.com.ar">piscologo online</a></h3>
+<h3><a href="https://proyectoasistir.com.ar">asistencia psicologica gratuita</a></h3>
+<h3><a href="https://proyectoasistir.com.ar">asistencia psicologica gratuita en buenos aires</a></h3>
+<h3><a href="https://proyectoasistir.com.ar">asistencia psicologica gratuita en capital federal</a></h3>
+<h3><a href="https://proyectoasistir.com.ar">psicologo gratis buenos aires</a></h3>
+<h3><a href="https://sastreriagiordano.com.ar">alquiler de trajes en tucuman</a></h3>
+<h3><a href="https://proyectoasistir.com.ar">piscologo online </a></h3>
+<h3><a href="https://sastreriagiordano.com.ar">alquiler de trajes para fiestas en tucuman</a></h3>
+<h3><a href="https://sastreriagiordano.com.ar">alquiler de trajes en tucuman</a></h3>
+<h3><a href="https://quepicada.com.ar">picadas en Tucuman</a></h3>
+<h3><a href="https://quepicada.com.ar">delivery de picadas en Tucuman</a></h3>
+<h3><a href="https://quepicada.com.ar">picadas a domicilio en Tucuman</a></h3>
+<h3><a href="https://quepicada.com.ar">picadas para regalar en Tucuman</a></h3>
+<h3><a href="https://quepicada.com.ar">regalos empresariales en Tucuman</a></h3>
+<h3><a href="https://quepicada.com.ar">box para regalar en tucuman</a></h3>
+<h3><a href="https://images.google.de/url?q=https://lacritica.com.ar/">noticias de tucuman</a></h3>
+<h3><a href="https://images.google.co.uk/url?q=https://lacritica.com.ar/">noticias de tucuman</a></h3>
+<h3><a href="https://images.google.co.jp/url?q=https://lacritica.com.ar/">tucuman</a></h3>
+<h3><a href="https://images.google.co.jp/url?q=https://proyectoasistir.com.ar">piscologo online</a></h3>
+<h3><a href="https://images.google.es/url?q=https://lacritica.com.ar/">ultimo momento tucuman</a></h3>
+<h3><a href="https://www.ted.com/profiles/28003350">TED</a></h3>
+<h3><a href="https://people.mozilla.org/p/sebasg">Mozilla</a></h3>
+<h3><a href="https://www.google.es/url?q=https://lacritica.com.ar/"> ricardo bussi </a></h3>
+<h3><a href="https://spark.adobe.com/page/QGyPFWsmiJCTK/">adobe </a></h3>
+<h3><a href="marketingdigitalexperience.com/curso-de-marketing-digital/">curso de marketing digital </a></h3>
+<h3><a href="https://huffduffer.com/sebasg/610279">huffduffer</a></h3>
+<h3><a href="https://spacebabe.ams3.digitaloceanspaces.com/index.html">Spaces babe</a></h3>
+<h3><a href="https://epaepa.oss-ap-northeast-1.aliyuncs.com/index.html">Epaepadiscoestepa</a></h3>
+<h3><a href="https://bit.ly/3eLLr0v">bitly</a></h3>
+<h3><a href="https://tucuman.hatenablog.com/entry/2021/05/15/133138">hatena</a></h3>
+<h3><a href="https://lacritica.com.ar/2021/05/16/barbie-beltramino/">Barbie Beltramino</a></h3>
+<h3><a href="https://ar.pinterest.com/LACRITICAok">pinterest</a></h3>
+<h3><a href="https://tucumaneando.vercel.app/">vercel</a></h3>
+<h3><a href="https://telekino.netlify.app/">quiniela</a></h3>
+<h3><a href="https://blog.goo.ne.jp/tucuman/e/2a37fd7d0413a9372510e9b8c13a057e">japan</a></h3>
+<h3><a href="https://grabcad.com/seba.gomes-1">grabcad</a></h3>
+<h3><a href="https://gfycat.com/@quinieladetucuman">twitaah</a></h3>
+<h3><a href="https://community.cisco.com/t5/user/viewprofilepage/user-id/1209451">cisco</a></h3>
+<h3><a href="https://issuu.com/lacriticadiario">issu</a></h3>
+<h3><a href="https://community.focusme.com/member.php?action=profile&uid=2910">focusme</a></h3>
+<h3><a href="https://www.producthunt.com/@sebasg">hunt</a></h3>
+<h3><a href="https://www.folkd.com/user/sebasg">folkd</a></h3>
+<h3><a href="http://gijonenelrecuerdo.elcomercio.es/2019/02/gijon-la-ciudad-industrial-burguesa-1880-1920.html#comment-2051">giron</a></h3>
+<h3><a href="https://seositecheckup.com/seo-audit/lacritica.com.ar">seo</a></h3>
+<h3><a href="https://demo-git-master-stucuman.vercel.app/">app</a></h3>
+<h3><a href="https://demo-stucuman.vercel.app/">vercell</a></h3>
+<h3><a href="https://tucumann.oss-ap-northeast-1.aliyuncs.com/index.html">alibababba</a></h3>
 
     <style jsx>{`
       :global(html, body) {
